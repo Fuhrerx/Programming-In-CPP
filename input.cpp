03fa5028484b49c5b4ab4ed363e9bb3c18fadbfa
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-01-25 11:17:27 +0530

    input

---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *arg[])
+{
+    int num;
+    cout << "enter the number : " << endl;
+    cin >> num;
+    cout << "entered number is : " << num << endl;
+
+    return 0;
+}
