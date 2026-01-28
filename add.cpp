d36638e1ceb17c59c392cacdb80fdd0b29a700db
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-01-28 19:27:04 +0530

    add.cpp

---
+++ add.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int a, b, sum;
+
+    std::cout << "Enter the first number : " << std::endl;
+    std::cin >> a;
+
+    std::cout << "Enter the second number : " << std::endl;
+    std::cin >> b;
+
+    sum = a + b;
+
+    std::cout << "Sum of two numbers " << a << " and " << b << " is : " << sum << std::endl;
+
+    return 0;
+}
