eb56b9bfd0d3394796eb84a5002e217bc8c9b656
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-01-28 18:08:17 +0530

    in.cpp

---
+++ in.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int i;
+
+    std::cout << "Enter the input value : " << std::endl;
+    std::cin >> i;
+
+    std::cout << i << std::endl;
+
+    return 0;
+}
