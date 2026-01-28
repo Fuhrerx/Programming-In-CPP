06e36cdf6186f3b7caf563fab64c8360a8a5abc4
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-01-28 18:08:32 +0530

    mulin.cpp

---
+++ mulin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::string name;
+    int age;
+
+    std::cout << "Enter name and age : " << std::endl;
+    std::cin >> name >> age;
+
+    std::cout << "Name : " << name << std::endl;
+    std::cout << "Age : " << age << std::endl;
+
+    return 0;
+}
