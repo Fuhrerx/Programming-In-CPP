5cf6b74dc12842d3866c8b030601299c767fc1cf
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-01-29 18:25:21 +0530

    areth.cpp

---
+++ areth.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+    int x = 5, y = 6, z = 7, w = 8;
+
+    std::cout << ++x << std::endl;
+    std::cout << --y << std::endl;
+
+    int a = 10, b = 3, sum, diff, mul, div, rem;
+
+    sum = a + b;
+    sum = a - b;
+    sum = a * b;
+    sum = a / b;
+    sum = a % b;
+
+    std::cout << "Sum : " << sum << std::endl;
+    std::cout << "Difference : " << diff << std::endl;
+    std::cout << "Product : " << mul << std::endl;
+    std::cout << "Quotient : " << div << std::endl;
+    std::cout << "Remainder : " << rem << std::endl;
+
+    return 0;
+}
