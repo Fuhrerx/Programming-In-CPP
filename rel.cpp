e43eff4845a32f0e1357eb8a94f35d7757c57496
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-01-29 18:26:12 +0530

    rel.cpp

---
+++ rel.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int x = 5;
+    int y = 3;
+
+    std::cout << (x == y) << std::endl;
+    std::cout << (x != y) << std::endl;
+    std::cout << (x > y) << std::endl;
+    std::cout << (x < y) << std::endl;
+    std::cout << (x >= y) << std::endl;
+    std::cout << (x <= y) << std::endl;
+
+    return 0;
+}
