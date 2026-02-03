e0f9e704884710ec815b1586b4ed783ceb821db3
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-02-03 16:44:53 +0530

    week.cpp

---
+++ misc/week.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Number of days in a week: " << 7 << endl;
+    cout << "1 - Sunday" << endl;
+    cout << "2 - Monday" << endl;
+    cout << "3 - Tuesday" << endl;
+    cout << "4 - Wednesday" << endl;
+    cout << "5 - Thursday" << endl;
+    cout << "6 - Friday" << endl;
+    cout << "7 - Saturday" << endl;
+    return 0;
+}
